d113e27556057ed6ae5252c248f74fb62c22c26a
Vishnu G <vishnugkannath@gmail.com>
2026-01-23 00:45:52 +0530

    addrd functions

---
+++ lessons/01_functions.py
@@ -0,0 +1,9 @@
+# functions = A block of reusable code place () after the function name to invoke it
+
+def happy_birthday(name, age): # parameter
+    print(f"Happy Birthday to {name}!")
+    print(f"You are {age} years old!")
+    print()
+
+happy_birthday("Bro", 20) # argument
+happy_birthday("Webz", 28)
