75c2bdeca1b178bfea96bf67a6c91c92843f1e0b
Vishnu G <vishnugkannath@gmail.com>
2026-01-30 14:44:12 +0530

    added python banking program

---
+++ exercises/03_python_banking_program.py
@@ -0,0 +1,56 @@
+# Python Banking Program
+
+def show_balance(balance):
+    print(f"Your balance is {balance:.2f}")
+
+def deposit():
+    amount = float(input("Enter an amount to be deposited: "))
+
+    if amount < 0:
+        print("That's not an valid amount")
+        return 0
+    else:
+        return amount
+
+
+def withdraw(balance):
+    amount = float(input("Enter amount to be withdrawn: "))
+
+    if amount > balance:
+        print("Insufficient funds")
+        return 0
+    elif amount < 0:
+        print("Amount must be greater than 0")
+        return 0
+    else:
+        return amount
+
+def main():
+
+    balance = 0
+    is_running = True
+
+    while is_running:
+        print("Banking Program")
+        print("1: Show Balance")
+        print("2: Deposit")
+        print("3: Withdraw")
+        print("4: Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            show_balance(balance)
+        elif choice == "2":
+            balance += deposit()
+        elif choice == "3":
+            balance -= withdraw(balance)
+        elif choice == "4":
+            is_running = False
+        else:
+            print("Please enter a valid choice")
+
+    print("Thankyou, Have a nice day!")
+
+if __name__ == "__main__":
+    main()
