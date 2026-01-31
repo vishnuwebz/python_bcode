85d2c38524a46afa0ea15c82166c722f802fd8a3
Vishnu G <vishnugkannath@gmail.com>
2026-01-31 15:52:24 +0530

    added encrypt program

---
+++ projects/05_encrypt_program.py
@@ -0,0 +1,36 @@
+import random
+import string
+
+# !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
+
+random.shuffle(key)
+
+print(f"chars: {chars}")
+print(f"key: {key}")
+
+# ENCRYPTION
+plain_text = input("Enter a message to encrypt: ")
+cipher_text = ""
+
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
+
+print(f"original message: {plain_text}")
+print(f"encrypted message: {cipher_text}")
+
+
+# DECRYPTION
+cipher_text = input("Enter a message to decrypt: ")
+plain_text = ""
+
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted message: {cipher_text}")
+print(f"original message: {plain_text}")
