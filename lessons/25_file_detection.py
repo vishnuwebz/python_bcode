8fc14a6af1ce5b5c5a08be2ba0b67df3f1cf9c4c
Vishnu G <vishnugkannath@gmail.com>
2026-02-06 00:13:19 +0530

    added basic file detection

---
+++ lessons/25_file_detection.py
@@ -0,0 +1,20 @@
+"""
+
+Basic file detection
+"""
+
+import os
+
+file_path = "text.txt"
+
+if os.path.exists(file_path):
+    print(f"The location '{file_path}' exists")
+
+    if os.path.isfile(file_path):
+        print("That is a file")
+    elif os.path.isdir(file_path):
+        print("That is a directory")
+else:
+    print(f"The location '{file_path}' does not exist")
+
+
